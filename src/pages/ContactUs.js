e98bb96f368583fe2c7d6aa447e8ec77598702d6
fazhenghan <hanfazheng@gmail.com>
2024-07-16 12:17:47 -0700

    Added placeholder in ContactUs page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './ContactUs.css';
 import ContactForm from '../components/ContactForm';
 
 const ContactUs = () => {
   return (
-    <div>
-      <ContactForm />
+    <div className="contact-us">
+      <div className="banner">
+        <img src="https://via.placeholder.com/1920x600" alt="Banner" className="banner-image" />
+      </div>
+      <div className="slogan-banner"> 
+        <h2>REACH OUT TODAY</h2>
+        <h3>Let’s talk about your project</h3>
+      </div>
+      <div>
+        <ContactForm />
+      </div>
     </div>
   );
 };
